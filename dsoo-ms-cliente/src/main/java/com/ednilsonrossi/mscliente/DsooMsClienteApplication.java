5e54c21f1bc37fd210a86f35520fb6fe8ab4ba95
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-18 18:48:44 -0300

    Criação do projeto do MS de Cliente

---
+++ dsoo-ms-cliente/src/main/java/com/ednilsonrossi/mscliente/DsooMsClienteApplication.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.mscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsooMsClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsooMsClienteApplication.class, args);
+	}
+
+}
---
+++ dsoo-ms-cliente/src/test/java/com/ednilsonrossi/mscliente/DsooMsClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.mscliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsooMsClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
