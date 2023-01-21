097b207fa97ec22287232380d82a4407ce75190c
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-21 09:21:04 -0300

    Criação do microsserviço financeiro

---
+++ dsoo-ms-financeiro/src/main/java/com/ednilsonrossi/msfinanceiro/DsooMsFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.msfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsooMsFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsooMsFinanceiroApplication.class, args);
+	}
+
+}
---
+++ dsoo-ms-financeiro/src/test/java/com/ednilsonrossi/msfinanceiro/DsooMsFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.msfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsooMsFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
