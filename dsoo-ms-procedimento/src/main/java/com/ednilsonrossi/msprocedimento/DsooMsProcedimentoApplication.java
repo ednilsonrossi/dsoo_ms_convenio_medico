ba9c1d15dd0420fcd3c348a611aeb32618761ccb
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-23 14:49:47 -0300

    criado projeto de MS Procedimento

---
+++ dsoo-ms-procedimento/src/main/java/com/ednilsonrossi/msprocedimento/DsooMsProcedimentoApplication.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.msprocedimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsooMsProcedimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsooMsProcedimentoApplication.class, args);
+	}
+
+}
---
+++ dsoo-ms-procedimento/src/test/java/com/ednilsonrossi/msprocedimento/DsooMsProcedimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.msprocedimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsooMsProcedimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
