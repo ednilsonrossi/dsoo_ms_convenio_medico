fab7aa9d088e1e8c162e8f4a1690e0a426d94431
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-24 18:52:30 -0300

    criado servidor de registros dos MS

---
+++ dsoo-ms-eureka-server/src/main/java/com/ednilsonrossi/eurekaserver/DsooMsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsooMsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsooMsEurekaServerApplication.class, args);
+	}
+
+}
