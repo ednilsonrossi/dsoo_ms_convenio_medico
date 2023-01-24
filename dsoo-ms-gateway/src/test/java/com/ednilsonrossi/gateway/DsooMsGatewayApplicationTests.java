3261af74608670001e065c1d859ade8ecdc555fb
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-01-24 20:20:38 -0300

    criação e configuração do servidor de gateway

---
+++ dsoo-ms-gateway/src/test/java/com/ednilsonrossi/gateway/DsooMsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednilsonrossi.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsooMsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
